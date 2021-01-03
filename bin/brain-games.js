099228909afb69d51268ba4137b841dc34346ed2
per0w <vladimirvpa@gmail.com>
2021-01-03 17:28:18 +0400

    step 4 - added github ci/cd, and configured eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { run } from "../src/cli.js";
+import run from '../src/cli.js';
 
 run();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const run = () => {
-    console.log('Welcome to the Brain Games!')
-    const name = readlineSync.question('Your answer: ');
-    console.log(`Hello, ${name}`);
-}
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}`);
+};
